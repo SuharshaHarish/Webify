300cbe4317b77b331ceeca02fd12fe97911c359f
Suharsha H <suharsha.gohan@gmail.com>
2020-11-08 09:53:37 +0530

    Final

--- codefury_backend/forms.py
+++ codefury_backend/forms.py
@@ -27,8 +27,6 @@ class RegistrationForm(UserCreationForm):
 
     def save(self,commit=True):
         user = super(RegistrationForm,self).save(commit=False)
-        user.first_name = self.cleaned_data['first_name']
-        user.last_name = self.cleaned_data['last_name']
         user.email = self.cleaned_data['email']
 
 
